5cc217a0719e2dab72fb01254e6d9a823f38c20c
MqheleD <ndabedd@gmail.com>
2025-05-28 21:57:56 +0200

    iOS Splash nevigation fix. Hopefully

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,18 @@
-// import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen.</Text>
-//     </View>
-//   );
-// }
+import SignIn from "./(forms)/SignIn"; // Assuming SignIn is the login form component
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check auth status and navigate if already logged in
+    // const isLoggedIn = checkAuthStatusSomehow();
+    router.replace("/(forms)/SignIn"); // Redirect to SignIn form
+    // if (isLoggedIn) {
+    // }
+  }, []);
+
+  return <SignIn />;
+}
