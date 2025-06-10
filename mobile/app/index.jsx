dcd41b9a073eaebc79c74e44dc70a3df4e1002dd
MqheleD <ndabedd@gmail.com>
2025-06-10 19:50:43 +0200

    iOS Navigation fix

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
 
-import SignIn from "./(forms)/SignIn"; // Assuming SignIn is the login form component
+import Login from "./(forms)/SignIn"; // Assuming SignIn is the login form component
 
 export default function Index() {
   const router = useRouter();
@@ -9,7 +9,7 @@ export default function Index() {
   useEffect(() => {
     // Check auth status and navigate if already logged in
     // const isLoggedIn = checkAuthStatusSomehow();
-    router.replace("/(forms)/SignIn"); // Redirect to SignIn form
+    router.replace("/(forms)/Login"); // Redirect to LogIn form
     // if (isLoggedIn) {
     // }
   }, []);
